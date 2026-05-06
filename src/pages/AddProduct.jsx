335d0a755e3c94e8d3355374a6c0f8ba9305b6d2
jihadable <umarjihad141004@gmail.com>
2026-05-06 20:43:24 +0700

    code on addproduct page

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,6 +1,6 @@
-import { IconPhoto, IconRuler2, IconShoppingBag, IconStack2, IconTag, IconTexture, IconWeight } from "@tabler/icons-react"
+import { IconChevronDown, IconClipboardText, IconPhoto, IconRuler2, IconShoppingBag, IconStack2, IconTag, IconTexture, IconWeight } from "@tabler/icons-react"
 import axios from "axios"
-import { useContext, useState } from "react"
+import { useContext, useEffect, useRef, useState } from "react"
 import { toast } from "react-toastify"
 import Loader from "../components/Loader"
 import Sidebar from "../components/Sidebar"
@@ -18,6 +18,47 @@ export default function AddProduct(){
 function Content(){
     const { user } = useContext(AuthContext)
     const [isLoading, setIsLoading] = useState(false)
+    const [categories, setCategories] = useState([])
+    const [selectedCategory, setSelectedCategory] = useState({
+        id: "",
+        name: "",
+        label: "None"
+    })
+    const [isCategoryOptionsShowed, setIsCategoryOptionsShowed] = useState(false)
+
+    const [
+        nameInputElement,
+        priceInputElement,
+        stockInputElement,
+        weightInputElement,
+        textureInputElement,
+        sizeInputElement
+    ] = [
+        useRef(null)
+    ]
+
+    useEffect(() => {
+        const getCategories = async() => {
+            try {
+                const APIEndpoint = import.meta.env.VITE_API_ENDPOINT
+                const { data } = await axios.get(`${APIEndpoint}/api/categories`)
+
+                setCategories(data.data.categories.map(category => {
+                    const label = category.name.charAt(0).toUpperCase() + category.name.slice(1)
+                    return {...category, label}
+                }))
+            } catch(error){
+                console.log(error)
+            }
+        }
+
+        getCategories()
+    }, [])
+
+    const updateSelectedCategory = (category) => {
+        setSelectedCategory(category)
+        setIsCategoryOptionsShowed(false)
+    }
 
     const handleAddProduct = async(event) => {
         try {
@@ -28,9 +69,9 @@ function Content(){
             const jwt = localStorage.getItem("token")
 
             const requestBody = {
-                name: categoryNameInputElement.current.value
+                
             }
-            await axios.post(`${APIEndpoint}/api/categories`, requestBody, {
+            await axios.post(`${APIEndpoint}/api/products`, requestBody, {
                 headers: {
                     "Authorization": `Bearer ${jwt}`
                 }
@@ -62,15 +103,15 @@ function Content(){
                         </article>
                         <article className="bg-primary/10 p-2 flex items-center gap-2 rounded-lg w-full">
                             <IconTag stroke={1.5} />
-                            <input type="text" placeholder="Price" className="bg-transparent outline-none w-full" required />
+                            <input type="number" min={1} placeholder="Price (Rp)" className="bg-transparent outline-none w-full" required />
                         </article>
                         <article className="bg-primary/10 p-2 flex items-center gap-2 rounded-lg w-full">
                             <IconStack2 stroke={1.5} />
-                            <input type="text" placeholder="Stock" className="bg-transparent outline-none w-full" required />
+                            <input type="number" min={1} placeholder="Stock" className="bg-transparent outline-none w-full" required />
                         </article>
                         <article className="bg-primary/10 p-2 flex items-center gap-2 rounded-lg w-full">
                             <IconWeight stroke={1.5} />
-                            <input type="text" placeholder="Weight" className="bg-transparent outline-none w-full" required />
+                            <input type="number" placeholder="Weight (kg)" className="bg-transparent outline-none w-full" required />
                         </article>
                         <article className="bg-primary/10 p-2 flex items-center gap-2 rounded-lg w-full">
                             <IconTexture stroke={1.5} />
@@ -86,6 +127,23 @@ function Content(){
                                 <p>Image</p>
                                 <input type="file" placeholder="Image" className="bg-transparent outline-none w-full" required hidden />
                             </label>
+                        </article>
+                        <article className="bg-primary/10 p-2 flex gap-2 rounded-lg w-full">
+                            <div className="flex">
+                                <IconClipboardText stroke={1.5} />
+                            </div>
+                            <textarea rows={7} placeholder="Description" className="bg-transparent outline-none w-full resize-none"></textarea>
+                        </article>
+                        <article className="flex relative">
+                            <button type="button" className="flex items-center gap-2 bg-primary p-2 rounded-lg text-white" onClick={() => setIsCategoryOptionsShowed(!isCategoryOptionsShowed)}>
+                                <span>Category: {selectedCategory.label}</span>
+                                <IconChevronDown stroke={1.5} className={`transition-all ${isCategoryOptionsShowed ? "rotate-180" : ""}`} />
+                            </button>
+                            <article className={`${isCategoryOptionsShowed ? "flex" : "hidden"} flex-col absolute top-full left-0 bg-white shadow-2xl rounded-md overflow-hidden`}>
+                            {categories.map((category, index) => (
+                                <button type="button" className="text-left p-2 whitespace-nowrap hover:bg-primary/5" key={index} onClick={() => updateSelectedCategory(category)}>{category.label}</button>
+                            ))}
+                            </article>
                         </article>
                     {
                         isLoading ?
